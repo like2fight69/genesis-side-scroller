ae1cfff75dd73ce4a5282a30e457a50748351639
Like2fight69 <tmsand23@gmail.com>
2023-04-26 18:40:25 -0400

    main menu

---
+++ inc/menu.h
@@ -0,0 +1,10 @@
+#ifndef _MENU_H_
+#define _MENU_H_
+
+Sprite* menu;
+Sprite* start;
+
+u16 MENU_init(u16 vramIndex);
+
+
+#endif
---
+++ src/menu.c
@@ -0,0 +1,19 @@
+
+#include "genesis.h"
+
+#include "menu.h"
+#include "collision.h"
+#include "level.h"
+#include "camera.h"
+#include "sfx.h"
+#include "utils.h"
+//#include "lifebar.h"
+#include "timer.h"
+#include "res_sprite.h"
+
+
+u16 MENU_init(u16 vramIndex)
+{
+
+
+}
